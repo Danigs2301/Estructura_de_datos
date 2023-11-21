60f9dba4f43771a182c6d25ec57303bd28361a23
v4nt3 <145690849+v4nt3@users.noreply.github.com>
2023-11-20 22:10:38 -0500

    Update clienteRegistro.py

--- clienteRegistro.py
+++ clienteRegistro.py
@@ -19,30 +19,21 @@ def buscar(lista, buscado): #buscar a los cliente por su identificacion
     return None
 
 lista = Lista()
-while True:
-    print("\nMenú de opciones:")
-    print("1. Registrar un cliente")
-    print("2. Buscar y mostrar un cliente")
-    print("3. Modificar información de un cliente")
-    print("4. Salir")
 
-    opcion = input("Seleccione una opción: ")
+def registrarCliente():
+    datos = {} #lista para almacenar los datos del cliente e insertarlos en la Lista()
+    nombre = input("Ingrese el nombre completo del cliente: ").capitalize() #.capitalize para poner la primera letra en mayuscula
+    cedula = input("Ingrese el número de identificación: ")
+    numero_pasaporte = input("Ingrese el número de pasaporte: ")
+    idiomas = input("Idiomas hablados (separados por comas): ").split(', ')
+    viaja_solo = input("¿Viaja solo? (Si/No): ").lower()
+    acompañantes = []
 
-    if opcion == "1":
-        datos = {} #lista para almacenar los datos del cliente e insertarlos en la Lista()
-        nombre = input("Ingrese el nombre completo del cliente: ").capitalize() #.capitalize para poner la primera letra en mayuscula
-        cedula = input("Ingrese el número de identificación: ")
-        numero_pasaporte = input("Ingrese el número de pasaporte: ")
-        idiomas = input("Idiomas hablados (separados por comas): ").split(', ')
-        viaja_solo = input("¿Viaja solo? (Si/No): ").lower() == 'si'
-        acompañantes = []
-
-        if not viaja_solo:
-            num_acompañantes = int(input("Número de personas que viajan con el cliente: "))
-            for i in range(num_acompañantes):
-                acompañante_nombre = input(f"Ingrese el nombre del acompañante {i + 1}: ").capitalize()
-                acompañantes.append(acompañante_nombre)
-
+    if viaja_solo == 'no':
+        num_acompañantes = int(input("Número de personas que viajan con el cliente: "))
+        for i in range(num_acompañantes):
+            acompañante_nombre = input(f"Ingrese el nombre del acompañante {i + 1}: ").capitalize()
+            acompañantes.append(acompañante_nombre)
         cliente = Cliente(nombre, cedula, numero_pasaporte, idiomas, viaja_solo, acompañantes)
 
         datos["Nombre"] = cliente.nombre
@@ -55,35 +46,99 @@ while True:
         insertar(lista, datos)
         print("¡Registro exitoso! El cliente ha sido registrado con éxito.")
         time.sleep(2) #demora el codigo para visualizar los mensajes
-    elif opcion == "2":
-        cedula = input("Ingrese el número de identificación del cliente: ")
-        clienteBuscado = buscar(lista, cedula)
-        if clienteBuscado: #si el cliente existe se muestran los datos
-            cliente = clienteBuscado.info
-            print(f"Nombre: {cliente['Nombre']}")
-            print(f"Cédula: {cliente['Cédula']}")
-            print(f"Número de Pasaporte: {cliente['Número de Pasaporte']}")
-            print(f"Idiomas Hablados: {', '.join(cliente['Idiomas Hablados'])}")
-            print(f"Viaja Solo: {'Sí' if cliente['Viaja Solo'] else 'No'}") #operador ternario
-                                #valor_verdadero if condicion else valor_falso, devuelve true o false dependiendo el valor tomado
+        
+    elif viaja_solo == 'si':
+        cliente = Cliente(nombre, cedula, numero_pasaporte, idiomas, viaja_solo, acompañantes)
 
-            if not cliente['Viaja Solo']:
-                print("Acompañantes:")
-                for i, acompañante in enumerate(cliente['Acompañantes'], 1): #enumertae recibe objetos iterables para enumerar
-                    print(f"{i}. {acompañante}")
-        else:
-            print("El número de identificación no coincide.")
-            time.sleep(2)
+        datos["Nombre"] = cliente.nombre
+        datos["Cédula"] = cliente.id
+        datos["Número de Pasaporte"] = cliente.numero_pasaporte
+        datos["Idiomas Hablados"] = cliente.idiomas
+        datos["Viaja Solo"] = cliente.viaja_solo
+        datos["Acompañantes"] = cliente.acompañantes
 
-    elif opcion == "3":
-        print("Modificar y actualizar información del cliente")
+        insertar(lista, datos)
+        print("¡Registro exitoso! El cliente ha sido registrado con éxito.")
+        time.sleep(2) #demora el codigo para visualizar los mensajes
 
-        id_modificar = input("Ingrese el número de identificación del cliente que desea modificar: ")
-        cliente_encontrado = buscar(lista, id_modificar)
+    else:
+        print('Ingrese una opcion correcta')
+        time.sleep(2)
 
-        if cliente_encontrado:
-            cliente = cliente_encontrado.info #se accede a la informacion del cliente
-            print("Cliente encontrado. Seleccione qué desea modificar:")
+
+
+def buscar_MostrarCliente ():
+    cedula = input("Ingrese el número de identificación del cliente: ")
+    clienteBuscado = buscar(lista, cedula)
+    if clienteBuscado: #si el cliente existe se muestran los datos
+        cliente = clienteBuscado.info
+        print(f"Nombre: {cliente['Nombre']}")
+        print(f"Cédula: {cliente['Cédula']}")
+        print(f"Número de Pasaporte: {cliente['Número de Pasaporte']}")
+        print(f"Idiomas Hablados: {', '.join(cliente['Idiomas Hablados'])}")
+        print(f"Viaja Solo: {'Sí' if cliente['Viaja Solo'] else 'No'}") #operador ternario
+                            #valor_verdadero if condicion else valor_falso, devuelve true o false dependiendo el valor tomado
+
+        if not cliente['Viaja Solo']:
+            print("Acompañantes:")
+            for i, acompañante in enumerate(cliente['Acompañantes'], 1): #enumertae recibe objetos iterables para enumerar
+                print(f"{i}. {acompañante}")
+    else:
+        print("El número de identificación no coincide.")
+        time.sleep(2)
+
+def modificarCliente ():
+    print("Modificar y actualizar información del cliente")
+
+    id_modificar = input("Ingrese el número de identificación del cliente que desea modificar: ")
+    cliente_encontrado = buscar(lista, id_modificar)
+
+    if cliente_encontrado:
+        cliente = cliente_encontrado.info #se accede a la informacion del cliente
+        print("Cliente encontrado. Seleccione qué desea modificar:")
+        print("1. Nombre")
+        print("2. Número de Pasaporte")
+        print("3. Idiomas Hablados")
+        print("4. ¿Viaja Solo?")
+        print("5. Volver al Menú Principal")
+
+        opc = input("Seleccione una opción: ")
+
+        while opc != "5": # se repite el menu hasta que el cliente decida regresar al menu principal
+            if opc == "1":
+                nuevo_nombre = input("Nuevo nombre: ").capitalize()
+                cliente['Nombre'] = nuevo_nombre
+                print("¡Cambios realizados con éxito!")
+                time.sleep(2)
+            elif opc == "2":
+                nuevo_numero_pasaporte = input("Nuevo número de pasaporte: ")
+                cliente['Número de Pasaporte'] = nuevo_numero_pasaporte
+                print("¡Cambios realizados con éxito!")
+                time.sleep(2)
+            elif opc == "3":
+                nuevos_idiomas = input("Nuevos idiomas hablados (separados por comas): ").split(', ')
+                cliente['Idiomas Hablados'] = nuevos_idiomas
+                print("¡Cambios realizados con éxito!")
+                time.sleep(2)
+            elif opc == "4":
+                nuevo_viaja_solo = input("¿Viaja solo? (Si/No): ").lower() == 'si'
+                cliente['Viaja Solo'] = nuevo_viaja_solo
+
+                if not nuevo_viaja_solo:
+                    num_acompañantes = int(input("Número de personas que viajan con el cliente: "))
+                    acompañantes = []
+                    for i in range(num_acompañantes):
+                        acompañante_nombre = input(f"Ingrese el nombre del acompañante {i + 1}: ").capitalize()
+                        acompañantes.append(acompañante_nombre)
+                    cliente['Acompañantes'] = acompañantes
+
+                print("¡Cambios realizados con éxito!")
+                time.sleep(2)
+
+            else:
+                print("Opción no válida")
+
+            print("\nSeleccione qué desea modificar:")
             print("1. Nombre")
             print("2. Número de Pasaporte")
             print("3. Idiomas Hablados")
@@ -92,57 +147,31 @@ while True:
 
             opc = input("Seleccione una opción: ")
 
-            while opc != "5": # se repite el menu hasta que el cliente decida regresar al menu principal
-                if opc == "1":
-                    nuevo_nombre = input("Nuevo nombre: ").capitalize()
-                    cliente['Nombre'] = nuevo_nombre
-                    print("¡Cambios realizados con éxito!")
-                    time.sleep(2)
+        print("Cargando menu principal")
+        time.sleep(2)
+    else:
+        print("El número de identificación no coincide")
+        time.sleep(2)
 
-                elif opc == "2":
-                    nuevo_numero_pasaporte = input("Nuevo número de pasaporte: ")
-                    cliente['Número de Pasaporte'] = nuevo_numero_pasaporte
-                    print("¡Cambios realizados con éxito!")
-                    time.sleep(2)
 
-                elif opc == "3":
-                    nuevos_idiomas = input("Nuevos idiomas hablados (separados por comas): ").split(', ')
-                    cliente['Idiomas Hablados'] = nuevos_idiomas
-                    print("¡Cambios realizados con éxito!")
-                    time.sleep(2)
 
-                elif opc == "4":
-                    nuevo_viaja_solo = input("¿Viaja solo? (Si/No): ").lower() == 'si'
-                    cliente['Viaja Solo'] = nuevo_viaja_solo
+while True:
+    print("\nMenú de opciones:")
+    print("1. Registrar un cliente")
+    print("2. Buscar y mostrar un cliente")
+    print("3. Modificar información de un cliente")
+    print("4. Salir")
 
-                    if not nuevo_viaja_solo:
-                        num_acompañantes = int(input("Número de personas que viajan con el cliente: "))
-                        acompañantes = []
-                        for i in range(num_acompañantes):
-                            acompañante_nombre = input(f"Ingrese el nombre del acompañante {i + 1}: ").capitalize()
-                            acompañantes.append(acompañante_nombre)
-                        cliente['Acompañantes'] = acompañantes
+    opcion = input("Seleccione una opción: ")
 
-                    print("¡Cambios realizados con éxito!")
-                    time.sleep(2)
+    if opcion == "1":
+        registrarCliente()
+        
+    elif opcion == "2":
+        buscar_MostrarCliente()
 
-                else:
-                    print("Opción no válida")
-
-                print("\nSeleccione qué desea modificar:")
-                print("1. Nombre")
-                print("2. Número de Pasaporte")
-                print("3. Idiomas Hablados")
-                print("4. ¿Viaja Solo?")
-                print("5. Volver al Menú Principal")
-
-                opc = input("Seleccione una opción: ")
-
-            print("Cargando menu principal")
-            time.sleep(2)
-        else:
-            print("El número de identificación no coincide")
-            time.sleep(2)
+    elif opcion == "3":
+        modificarCliente()
 
     elif opcion == "4":
         print("Saliendo del programa.")
